6ec873b984a649e171535c1c2f8bcc87fe650bf9
mattkohl <m.kohl@geophy.com>
2018-03-05 09:59:40 +0000

    Initial commit

---
+++ src/main/scala/com/mattkohl/nlp/Main.scala
@@ -0,0 +1,23 @@
+package com.mattkohl.nlp
+
+import cats.effect.IO
+import fs2.StreamApp
+import io.circe._
+import org.http4s._
+import org.http4s.circe._
+import org.http4s.dsl.Http4sDsl
+import org.http4s.server.blaze.BlazeBuilder
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Main extends StreamApp[IO] with Http4sDsl[IO] {
+  val service = HttpService[IO] {
+    case GET -> Root / "hello" / name =>
+      Ok(Json.obj("message" -> Json.fromString(s"Hello, ${name}")))
+  }
+
+  def stream(args: List[String], requestShutdown: IO[Unit]) =
+    BlazeBuilder[IO]
+      .bindHttp(8080, "0.0.0.0")
+      .mountService(service, "/")
+      .serve
+}
